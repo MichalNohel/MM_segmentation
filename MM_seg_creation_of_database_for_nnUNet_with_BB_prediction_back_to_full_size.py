3561837c6e4ffd48d2cc0bc6dd4b07fda745c02b
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2024-04-22 10:11:10 +0200

    skript to get full size image for nnUNet_v_2_0 final

--- MM_seg_creation_of_database_for_nnUNet_with_BB_prediction_back_to_full_size.py
+++ MM_seg_creation_of_database_for_nnUNet_with_BB_prediction_back_to_full_size.py
@@ -34,34 +34,29 @@ if __name__ == "__main__":
     predict_pacients=['Myel_012', 'Myel_018', 'Myel_023', 'Myel_024', 'Myel_043', 'Myel_047', 'Myel_052', 'Myel_059', 'Myel_069', 'Myel_070']
     for t in predict_pacients: 
         current_path=join(path_to_crop_parameters,t)
-    f=open(current_path+'.json')
-    data = json.load(f)
-    orig_size=data['orig_size']
-    min_coords=data['min_coords']
-    max_coords=data['max_coords']
-    f.close()
+        #open json file and get coordinates
+        f=open(current_path+'.json')
+        data = json.load(f)
+        orig_size=data['orig_size']
+        min_coords=data['min_coords']
+        max_coords=data['max_coords']
+        f.close()
     
-    t=predict_pacients[0]
-    #VMI 40keV
-    vmi_40kev=subfiles(join(base, t, 'VMI'), join=False, suffix="40kev.nii.gz")[0] 
-    curr = join(base, t, 'VMI')
-    image_file = join(curr, vmi_40kev)
-    img = nib.load(image_file)
-    
-    predicted_file=path_to_predicted_segmentations+'/Predikce_follow_ups/'+t+'.nii.gz'
-    predicted_mask = nib.load(predicted_file)
-    predicted_mask_data=predicted_mask.get_fdata()
-    
-    full_size_mask=np.zeros(orig_size,dtype=bool)
-    full_size_mask[min_coords[0]:max_coords[0]+1, min_coords[1]:max_coords[1]+1, min_coords[2]:max_coords[2]+1]=predicted_mask_data
-    
-
-    
-      
-    
-    
-    pom_full_size_mask = nib.Nifti1Image(full_size_mask, img.affine, img.header)
-    nib.save(pom_full_size_mask, join(crop_parameters_folder, t + version_name + ".nii.gz"))
+        #VMI 40keV - load nifti info
+        vmi_40kev=subfiles(join(base, t, 'VMI'), join=False, suffix="40kev.nii.gz")[0] 
+        curr = join(base, t, 'VMI')
+        image_file = join(curr, vmi_40kev)
+        img = nib.load(image_file)
+        #load predicted data
+        predicted_file=path_to_predicted_segmentations+'/Predikce_follow_ups/'+t+'.nii.gz'
+        predicted_mask = nib.load(predicted_file)
+        predicted_mask_data=predicted_mask.get_fdata()
+        #create full size image
+        full_size_mask=np.zeros(orig_size,dtype=bool)
+        full_size_mask[min_coords[0]:max_coords[0]+1, min_coords[1]:max_coords[1]+1, min_coords[2]:max_coords[2]+1]=predicted_mask_data
+        #save full size image
+        pom_full_size_mask = nib.Nifti1Image(full_size_mask, img.affine, img.header)
+        nib.save(pom_full_size_mask, join(crop_parameters_folder, t + version_name + ".nii.gz"))
     
     
     
