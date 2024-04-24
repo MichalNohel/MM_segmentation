fac4808866e31fcf5177cd6a40c424423bb77640
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2024-04-24 13:04:10 +0200

    update to saving masks directly to database

--- Segmentation_of_trabecular_spine_LumVBCanSeg/Lumbar_Spine_seg_LumVBCanSeg_creation_of_database_for_nnUNet_prediction_back_to_full_size.py
+++ Segmentation_of_trabecular_spine_LumVBCanSeg/Lumbar_Spine_seg_LumVBCanSeg_creation_of_database_for_nnUNet_prediction_back_to_full_size.py
@@ -17,6 +17,7 @@ import numpy as np
 #%%
 
 if __name__ == "__main__":
+    '''
     base = 'E:/Znaceni_dat/Data/'   
     task_id = 908
     version_name = "_trabecular_seg_nnUNet_v_1_0"
@@ -58,7 +59,50 @@ if __name__ == "__main__":
         pom_full_size_mask = nib.Nifti1Image(full_size_mask, img.affine, img.header)
         nib.save(pom_full_size_mask, join(crop_parameters_folder, t + version_name + ".nii.gz"))
  
+    '''
+    # verze přímo s nahráním do databáze
+    base = 'E:\Analysis_of_MM_patients/'   
+    task_id = 908
+    version_name = "_trabecular_seg_nnUNet_v_1_0"
+    task_name = "Lumbar_spine_seg" 
+    foldername = "Dataset%03.0d_%s" % (task_id, task_name) 
+    path_to_crop_parameters = join('E://nnUNet_v2_MAIN_FILE/nnUNet_raw', foldername,'crop_parameters/')  
+    
+    # %% folder with predicted data from nnUNet
+    path_to_predicted_segmentations=join('E://nnUNet_v2_MAIN_FILE/nnUNet_results/predict', foldername)
     
     
+    
+    predict_pacients=['Healthy_006','Healthy_007','Healthy_008','Healthy_009','Healthy_010','Myel_001', 'Myel_002', 'Myel_003', 'Myel_004', 'Myel_005']
+    for t in predict_pacients: 
+        current_path=join(path_to_crop_parameters,t)
+        #open json file and get coordinates
+        f=open(current_path+'.json')
+        data = json.load(f)
+        orig_size=data['orig_size']
+        min_coords=data['min_coords']
+        max_coords=data['max_coords']
+        f.close()
+        
+        # output folder for full size image
+        crop_parameters_folder = join(base,t,'Spine_labels','Trabecular_masks') 
+        maybe_mkdir_p(crop_parameters_folder) 
+        
+        #VMI 40keV - load nifti info
+        vmi_40kev=subfiles(join(base, t, 'VMI'), join=False, suffix="40kev.nii.gz")[0] 
+        curr = join(base, t, 'VMI')
+        image_file = join(curr, vmi_40kev)
+        img = nib.load(image_file)
+        #load predicted data
+        predicted_file=path_to_predicted_segmentations+'/Predict_clin_data_crop_rotovane_zpet/'+t+'.nii.gz'
+        predicted_mask = nib.load(predicted_file)
+        predicted_mask_data=predicted_mask.get_fdata()
+        #create full size image
+        full_size_mask=np.zeros(orig_size)
+        full_size_mask[min_coords[0]:max_coords[0]+1, min_coords[1]:max_coords[1]+1, min_coords[2]:max_coords[2]+1]=predicted_mask_data
+        #save full size image
+        pom_full_size_mask = nib.Nifti1Image(full_size_mask, img.affine, img.header)
+        nib.save(pom_full_size_mask, join(crop_parameters_folder, t + version_name + ".nii.gz"))
+ 
     
     
