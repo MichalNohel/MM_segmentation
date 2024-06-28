8360b12e458c0dd1e1ad47226556d16fe4460dfb
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2024-06-28 13:59:06 +0200

    final

--- Controling_of_segmentation_of_database_final.m
+++ Controling_of_segmentation_of_database_final.m
@@ -9,10 +9,6 @@ ID_patient='S80060';
 path_to_orig_data=[path_to_database ID_patient];
 DATA=load_dicom_series(path_to_orig_data,patient);
 imshow5(DATA)
-%%
-
-
-
 
 %% Myel_002
 patient='002'
@@ -20,4 +16,497 @@ ID_patient='S84960';
 path_to_orig_data=[path_to_database ID_patient];
 DATA=load_dicom_series(path_to_orig_data,patient);
 imshow5(DATA)
+
+%% Myel_003
+patient='003'
+ID_patient='S98240';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_004
+patient='004'
+ID_patient='S2470';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_005
+patient='005'
+ID_patient='S17160';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_006
+patient='006'
+ID_patient='S32070';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_007
+patient='007'
+ID_patient='S54240';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_008
+patient='008'
+ID_patient='S45700';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_009
+patient='009'
+ID_patient='S31610';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_010
+patient='010'
+ID_patient='S55480';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_011
+patient='011'
+ID_patient='S40510';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_012
+patient='012'
+ID_patient='S58550';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_014
+patient='014'
+ID_patient='S71550';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_015
+patient='015'
+ID_patient='S70810';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_016
+patient='016'
+ID_patient='S70120';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_017
+patient='017'
+ID_patient='S79990';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_018
+patient='018'
+ID_patient='S35360';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_019
+patient='019'
+ID_patient='S94100';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_020
+patient='020'
+ID_patient='S1710';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_021
+patient='021'
+ID_patient='S32410';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_022
+patient='022'
+ID_patient='S68450';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_023
+patient='023'
+ID_patient='S17100';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_024
+patient='024'
+ID_patient='S42810';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_025
+patient='025'
+ID_patient='S80230';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_026
+patient='026'
+ID_patient='S69520';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_027
+patient='027'
+ID_patient='S92250';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_028
+patient='028'
+ID_patient='S41610';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_029
+patient='029'
+ID_patient='S63730';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_030
+patient='030'
+ID_patient='S43120';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_031
+patient='031'
+ID_patient='S32900';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_033
+patient='033'
+ID_patient='S45980';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_034
+patient='034'
+ID_patient='S58070';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_036
+patient='036'
+ID_patient='S54970';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_038
+patient='038'
+ID_patient='S87280';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_039
+patient='039'
+ID_patient='S16480';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_040
+patient='040'
+ID_patient='S18320';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_041
+patient='041'
+ID_patient='S18740';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_042
+patient='042'
+ID_patient='S88520';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_043
+patient='043'
+ID_patient='S19420';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_044
+patient='044'
+ID_patient='S19730';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_045
+patient='045'
+ID_patient='S21770';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_046
+patient='046'
+ID_patient='S23400';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_047
+patient='047'
+ID_patient='S88810';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_048
+patient='048'
+ID_patient='S25650';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_050
+patient='050'
+ID_patient='S93130';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_051
+patient='051'
+ID_patient='S94780';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_052
+patient='052'
+ID_patient='S93800';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_053
+patient='053'
+ID_patient='S95130';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_054
+patient='054'
+ID_patient='S95290';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_055
+patient='055'
+ID_patient='S95910';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_056
+patient='056'
+ID_patient='S840';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_057
+patient='057'
+ID_patient='S97310';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_058
+patient='058'
+ID_patient='S98080';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_059
+patient='059'
+ID_patient='S98960';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_060
+patient='060'
+ID_patient='S98820';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_061
+patient='061'
+ID_patient='S99420';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_062
+patient='062'
+ID_patient='S2200';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_063
+patient='063'
+ID_patient='S7910';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_064
+patient='064'
+ID_patient='S9410';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_065
+patient='065'
+ID_patient='S5990';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_066
+patient='066'
+ID_patient='S11520';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_067
+patient='067'
+ID_patient='S12890';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_068
+patient='068'
+ID_patient='S13000';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_069
+patient='069'
+ID_patient='S13070';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_070
+patient='070'
+ID_patient='S14180';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_071
+patient='071'
+ID_patient='S14610';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_072
+patient='072'
+ID_patient='S15210';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_077
+patient='077'
+ID_patient='S47870';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_078
+patient='078'
+ID_patient='S52480';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_079
+patient='079'
+ID_patient='S52060';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_080
+patient='080'
+ID_patient='S52950';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+%% Myel_081
+patient='081'
+ID_patient='S56390';
+path_to_orig_data=[path_to_database ID_patient];
+DATA=load_dicom_series(path_to_orig_data,patient);
+imshow5(DATA)
+
+
+
 
